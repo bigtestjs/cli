34811374776ec6fb68d2e98f8fc157105560b603
Wil Wilsman <wil@frontside.io>
2018-08-21 13:51:05 -0500

    Improve help output

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 #! /usr/bin/env node
 
+const chalk = require('chalk');
+
+require('yargonaut')
+  .style('red.bold', [
+    'Did you mean %s?'
+  ])
+  .style('blue', [
+    'aliases:',
+    'default:',
+    'boolean',
+    'number',
+    'string'
+  ])
+  .style('green.bold', [
+    'Commands:',
+    'Options:',
+    'Client Options:',
+    'Proxy Options:',
+    'Adapter Options:'
+  ]);
+
 require('yargs')
   .scriptName('bigtest')
+  .usage(chalk`{green.bold Usage:} $0 <command>`)
   .version(require('./package.json').version)
-  .command('init', false, require('./dist/init'))
-  .command('run', false, require('./dist/run'))
-  .fail(() => process.exit(1))
+  .command(require('./dist/init'))
+  .command(require('./dist/run'))
+  .demandCommand(1, '')
+  .recommendCommands()
+  .fail((msg, err, yargs) => {
+    if (!msg) {
+      console.log(yargs.help());
+      process.exit();
+    } else if (err) {
+      console.error(err);
+      process.exit(1);
+    } else {
+      console.error(msg);
+      process.exit(1);
+    }
+  })
   .help()
   .wrap(null)
   .parse();
