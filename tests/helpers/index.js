25f868bb9fd3302708b4217d7ce8084d2035a133
Wil Wilsman <wil@frontside.io>
2018-09-04 19:21:24 -0500

    Add serve plugin tests

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,7 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-
-chai.use(chaiAsPromised);
+import logger from '@util/logger';
 
 export { expect } from 'chai';
 export { when } from '@bigtest/convergence';
@@ -11,3 +10,9 @@ export { default as request } from '@run/util/request';
 export { default as defer } from './defer';
 export { default as dedent } from './dedent';
 export { default as readFile } from './read-file';
+
+// use chai-as-promised
+chai.use(chaiAsPromised);
+
+// silence the global logger
+logger.level = 'silent';
